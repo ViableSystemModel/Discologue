c13455ce631a0b90e6dd1f7ae1ead3fba78be7f4
ViableSystemModel <44686976+ViableSystemModel@users.noreply.github.com>
2026-08-12 23:36:32 -0400

    Debug  environment variables

--- discologue-editor/src/env.ts
+++ discologue-editor/src/env.ts
@@ -1,16 +1,23 @@
 import { createEnv } from "@t3-oss/env-core";
 import * as v from 'valibot'
 
-export const env = createEnv({
-  runtimeEnv: import.meta.env,
-  emptyStringAsUndefined: true,
-  clientPrefix: "VITE_",
-  client: {
-    VITE_CLERK_PUBLISHABLE_KEY: v.pipe(v.string(), v.minLength(1)),
-    VITE_SPACETIMEDB_HOST: v.optional(v.pipe(v.string(), v.url()), 'http://localhost:3001'),
-    VITE_SPACETIMEDB_DB_NAME: v.optional(v.pipe(v.string(), v.minLength(1)), 'discologue'),
-  },
-  server: {
-    CLERK_SECRET_KEY: v.optional(v.pipe(v.string(), v.minLength(1))),
-  },
-});
+export let env: { [key: string]: string };
+
+try {
+  env = createEnv({
+    runtimeEnv: import.meta.env,
+    emptyStringAsUndefined: true,
+    clientPrefix: "VITE_",
+    client: {
+      VITE_CLERK_PUBLISHABLE_KEY: v.pipe(v.string(), v.minLength(1)),
+      VITE_SPACETIMEDB_HOST: v.optional(v.pipe(v.string(), v.url()), 'http://localhost:3001'),
+      VITE_SPACETIMEDB_DB_NAME: v.optional(v.pipe(v.string(), v.minLength(1)), 'discologue'),
+    },
+    server: {
+      CLERK_SECRET_KEY: v.optional(v.pipe(v.string(), v.minLength(1))),
+    },
+  });
+} catch {
+  env = {}
+  throw new Error('import.meta.env: ' + JSON.stringify(import.meta.env))
+}
